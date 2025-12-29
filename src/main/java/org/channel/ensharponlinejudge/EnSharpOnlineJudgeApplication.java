008a707ac43a957d7100187a83687682e518367d
SangHyeok0908 <tkdgur9799@gmail.com>
2025-12-29 15:27:01 +0900

    init: project initialize

---
+++ src/main/java/org/channel/ensharponlinejudge/EnSharpOnlineJudgeApplication.java
@@ -0,0 +1,12 @@
+package org.channel.ensharponlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnSharpOnlineJudgeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EnSharpOnlineJudgeApplication.class, args);
+  }
+}
---
+++ src/test/java/org/channel/ensharponlinejudge/EnSharpOnlineJudgeApplicationTests.java
@@ -0,0 +1,11 @@
+package org.channel.ensharponlinejudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnSharpOnlineJudgeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}

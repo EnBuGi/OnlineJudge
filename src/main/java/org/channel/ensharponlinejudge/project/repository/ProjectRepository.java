6c21425152d8199363866bc7023d05bdee86c013
vvineey <vvin506@naver.com>
2026-02-14 02:17:10 +0900

    Feat : Project Repository 추가

---
+++ src/main/java/org/channel/ensharponlinejudge/project/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package org.channel.ensharponlinejudge.project.repository;
+
+import org.channel.ensharponlinejudge.project.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+}

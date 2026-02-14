c859885c1046b5357b3240ad51db5678fca3e7c5
vvineey <vvin506@naver.com>
2026-02-15 00:54:18 +0900

    Feat : 채점 중인 제출이 있으면 409 예외 처리

--- src/main/java/org/channel/ensharponlinejudge/submission/service/SubmissionService.java
+++ src/main/java/org/channel/ensharponlinejudge/submission/service/SubmissionService.java
@@ -1,11 +1,13 @@
 package org.channel.ensharponlinejudge.submission.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.channel.ensharponlinejudge.exception.BusinessException;
 import org.channel.ensharponlinejudge.exception.enums.SubmissionErrorCode;
 import org.channel.ensharponlinejudge.project.domain.Project;
 import org.channel.ensharponlinejudge.project.repository.ProjectRepository;
+import org.channel.ensharponlinejudge.submission.domain.SubmissionStatus;
 import org.channel.ensharponlinejudge.submission.repository.SubmissionRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +18,20 @@ import lombok.RequiredArgsConstructor;
 public class SubmissionService {
 
   private final ProjectRepository projectRepository;
-  private final SubmissionRepository SubmissionRepository;
+  private final SubmissionRepository submissionRepository;
 
   public void submit(UUID userId, UUID projectId, String repoUrl) {
 
-    Project project = projectRepository.findById(projectId)
+    List<SubmissionStatus> inProgress =
+        List.of(SubmissionStatus.QUEUED, SubmissionStatus.PROCESSING);
+
+    Project project =
+        projectRepository
+            .findById(projectId)
             .orElseThrow(() -> new BusinessException(SubmissionErrorCode.PROJECT_NOT_FOUND));
+
+    if (submissionRepository.existsByUserIdAndProjectIdAndStatusIn(userId, projectId, inProgress)) {
+      throw new BusinessException(SubmissionErrorCode.SUBMISSION_IN_PROGRESS);
+    }
   }
 }

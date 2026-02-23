352354414da8b1b184e49f90baebf4f40f09604b
vvineey <vvin506@naver.,com>
2026-02-24 00:18:21 +0900

    Feat : 이벤트 dto 추가

--- src/main/java/org/channel/ensharponlinejudge/submission/infra/queue/dto/SubmissionCreatedEvent.java
+++ src/main/java/org/channel/ensharponlinejudge/submission/infra/queue/dto/SubmissionCreatedEvent.java
@@ -2,7 +2,7 @@ package org.channel.ensharponlinejudge.submission.infra.queue.dto;
 
 import java.util.UUID;
 
-public record SubmissionQueueEvent(
+public record SubmissionCreatedEvent(
         UUID submissionId
 ) {
 }

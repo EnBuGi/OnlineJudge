fe372977093307d0fb3e7b3565cf94b0f00c2834
vvineey <vvin506@naver.,com>
2026-03-05 17:35:22 +0900

    Fix : 제출 성공 시 제출 ID 반환하도록 수정 - 200 -> 201로 수정

--- src/main/java/org/channel/ensharponlinejudge/submission/presentation/SubmissionController.java
+++ src/main/java/org/channel/ensharponlinejudge/submission/presentation/SubmissionController.java
@@ -1,8 +1,11 @@
 package org.channel.ensharponlinejudge.submission.presentation;
 
+import java.util.UUID;
+
 import jakarta.validation.Valid;
 
 import org.channel.ensharponlinejudge.submission.presentation.dto.request.SubmitRequest;
+import org.channel.ensharponlinejudge.submission.presentation.dto.response.SubmitResponse;
 import org.channel.ensharponlinejudge.submission.service.SubmissionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +23,11 @@ public class SubmissionController {
   private final SubmissionService submissionService;
 
   @PostMapping
-  public ResponseEntity<Void> submit(@Valid @RequestBody SubmitRequest submitRequest) {
-    submissionService.submit(
-        submitRequest.userId(), submitRequest.projectId(), submitRequest.repoUrl());
-    return ResponseEntity.ok().build();
+  public ResponseEntity<SubmitResponse> submit(@Valid @RequestBody SubmitRequest submitRequest) {
+    UUID submissionId =
+        submissionService.submit(
+            submitRequest.userId(), submitRequest.projectId(), submitRequest.repoUrl());
+
+    return ResponseEntity.status(201).body(new SubmitResponse(submissionId));
   }
 }

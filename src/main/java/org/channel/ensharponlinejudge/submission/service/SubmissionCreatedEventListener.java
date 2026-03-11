5043845db07ffd9f3e349b6bebfbacd8efb2219a
vvineey <vvin506@naver.com>
2026-03-12 01:58:52 +0900

    Fix : SubmissionCreatedEventListener 내부 에러 코드 오타

--- src/main/java/org/channel/ensharponlinejudge/submission/service/SubmissionCreatedEventListener.java
+++ src/main/java/org/channel/ensharponlinejudge/submission/service/SubmissionCreatedEventListener.java
@@ -1,15 +1,13 @@
 package org.channel.ensharponlinejudge.submission.service;
 
-
-import lombok.RequiredArgsConstructor;
 import org.channel.ensharponlinejudge.exception.BusinessException;
 import org.channel.ensharponlinejudge.exception.enums.SubmissionErrorCode;
 import org.channel.ensharponlinejudge.project.domain.Project;
 import org.channel.ensharponlinejudge.project.repository.ProjectRepository;
 import org.channel.ensharponlinejudge.submission.domain.Submission;
 import org.channel.ensharponlinejudge.submission.infra.queue.SubmissionQueuePublisher;
-import org.channel.ensharponlinejudge.submission.infra.queue.dto.SubmissionJudgeRequest;
 import org.channel.ensharponlinejudge.submission.infra.queue.dto.SubmissionCreatedEvent;
+import org.channel.ensharponlinejudge.submission.infra.queue.dto.SubmissionJudgeRequest;
 import org.channel.ensharponlinejudge.submission.repository.SubmissionRepository;
 import org.channel.ensharponlinejudge.user.domain.User;
 import org.channel.ensharponlinejudge.user.repository.UserRepository;
@@ -18,54 +16,54 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
 @RequiredArgsConstructor
 public class SubmissionCreatedEventListener {
 
-    private final SubmissionRepository submissionRepository;
-    private final ProjectRepository projectRepository;
-    private final UserRepository userRepository;
-    private final SubmissionQueuePublisher submissionQueuePublisher;
+  private final SubmissionRepository submissionRepository;
+  private final ProjectRepository projectRepository;
+  private final UserRepository userRepository;
+  private final SubmissionQueuePublisher submissionQueuePublisher;
 
-    @Transactional
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void onSubmissionQueued(SubmissionCreatedEvent submissionCreatedEvent){
+  @Transactional
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void onSubmissionQueued(SubmissionCreatedEvent submissionCreatedEvent) {
 
-        Submission submission = submissionRepository.
-                findById(submissionCreatedEvent.submissionId())
-                .orElseThrow(()-> new BusinessException(SubmissionErrorCode.SUBMISSON_NOT_FOUND));
+    Submission submission =
+        submissionRepository
+            .findById(submissionCreatedEvent.submissionId())
+            .orElseThrow(() -> new BusinessException(SubmissionErrorCode.SUBMISSION_NOT_FOUND));
 
+    Project project =
+        projectRepository
+            .findById(submission.getProjectId())
+            .orElseThrow(() -> new BusinessException(SubmissionErrorCode.PROJECT_NOT_FOUND));
 
-        Project project =
-                projectRepository
-                        .findById(submission.getProjectId())
-                        .orElseThrow(() -> new BusinessException(SubmissionErrorCode.PROJECT_NOT_FOUND));
+    User user =
+        userRepository
+            .findById(submission.getUserId())
+            .orElseThrow(() -> new BusinessException(SubmissionErrorCode.USER_NOT_FOUND));
 
+    SubmissionJudgeRequest submissionJudgeRequest =
+        new SubmissionJudgeRequest(
+            submission.getId(),
+            submission.getUserId(),
+            submission.getProjectId(),
+            submission.getRepoUrl(),
+            project.getTestCodeUrl(),
+            project.getTimeLimit(),
+            project.getMemoryLimit());
+    try {
+      submissionQueuePublisher.enqueue(submissionJudgeRequest);
 
-        User user =
-                userRepository
-                        .findById(submission.getUserId())
-                        .orElseThrow(() -> new BusinessException(SubmissionErrorCode.USER_NOT_FOUND));
+      // 제출 상태 QUEUED로 변경
+      submission.markQueued();
+    } catch (Exception e) {
 
-
-        SubmissionJudgeRequest submissionJudgeRequest =
-                new SubmissionJudgeRequest(
-                        submission.getId(),
-                        submission.getUserId(),
-                        submission.getProjectId(),
-                        submission.getRepoUrl(),
-                        project.getTestCodeUrl(),
-                        project.getTimeLimit(),
-                        project.getMemoryLimit());
-        try {
-            submissionQueuePublisher.enqueue(submissionJudgeRequest);
-
-            //제출 상태 QUEUED로 변경
-            submission.markQueued();
-        } catch (Exception e) {
-
-            //제출 상태 SYSTEM_ERROR로 변경
-           submission.markSystemError();
-        }
+      // 제출 상태 SYSTEM_ERROR로 변경
+      submission.markSystemError();
     }
+  }
 }

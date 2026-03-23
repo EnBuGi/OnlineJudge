48e8f2776e74ff6f3239fef296843a8ed7a85ab3
huro0906 <huro0906@gmail.com>
2026-03-23 18:02:33 +0900

    [Security] DTO 내 민감 정보(GitHub AT) 로깅 방지를 위한 마스킹 처리

--- src/main/java/org/channel/ensharponlinejudge/submission/infra/queue/dto/SubmissionJudgeRequest.java
+++ src/main/java/org/channel/ensharponlinejudge/submission/infra/queue/dto/SubmissionJudgeRequest.java
@@ -1,6 +1,9 @@
 package org.channel.ensharponlinejudge.submission.infra.queue.dto;
 
+import java.util.List;
 import java.util.UUID;
+
+import org.channel.ensharponlinejudge.project.presentation.dto.request.TestCaseDto;
 
 public record SubmissionJudgeRequest(
     UUID submissionId,
@@ -10,4 +13,22 @@ public record SubmissionJudgeRequest(
     String testCodeUrl,
     Integer timeLimit,
     Integer memoryLimit,
-    String projectType) {}
+    String projectType,
+    List<TestCaseDto> testCases,
+    String githubAccessToken) {
+  @Override
+  public String toString() {
+    return "SubmissionJudgeRequest[" +
+        "submissionId=" + submissionId +
+        ", userId=" + userId +
+        ", projectId=" + projectId +
+        ", repoUrl=" + repoUrl +
+        ", testCodeUrl=" + testCodeUrl +
+        ", timeLimit=" + timeLimit +
+        ", memoryLimit=" + memoryLimit +
+        ", projectType=" + projectType +
+        ", testCases=" + testCases +
+        ", githubAccessToken=[PROTECTED]" +
+        "]";
+  }
+}
